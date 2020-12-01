b1a378819ccbe396e6da99ba1f3d3bc96b903fc5
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-01 12:07:21 +0100

    Day 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,5 +1,29 @@
 class Day01 {
-    fun firstTask(input : Int) : Int {
-        return 0
+    fun productOfTwo2020Parts(numbers: List<Int>) : Int {
+        val lookingFor = HashSet<Int>()
+        for (number in numbers) {
+            if (lookingFor.contains(number)) {
+                return number * (2020 - number)
+            }
+            lookingFor.add(2020 - number)
+        }
+        return -1
+    }
+
+    fun productOfThree2020Parts(numbers: List<Int>) : Int {
+        val lastIndex = numbers.size
+        for ((index, number) in numbers.withIndex()) {
+            for ((otherIndex, otherNumber) in numbers.subList(index + 1, lastIndex).withIndex()) {
+                if (number + otherNumber <= 2020) {
+                    for (thirdNumber in numbers.subList(otherIndex + 1, lastIndex)) {
+                        if (number + otherNumber + thirdNumber == 2020) {
+                            return number * otherNumber * thirdNumber
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -1,12 +1,44 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.toList
 
 internal class Day01Test {
 
     @ParameterizedTest
-    @CsvSource("0, 1")
-    fun firstTask(input: Int, expected : Int) {
-        assertEquals(expected, Day01().firstTask(input));
+    @CsvSource(value = [
+        "1721, 979, 366, 299, 675, 1456; 514579",
+        "1, 2019; 2019",
+        "1010, 0, 1010; 1020100"
+    ], delimiter = ';')
+    fun productOfTwo2020Parts(rawNumbers: String, expected : Int) {
+        val numbers = rawNumbers.split(", ").map{ it.toInt()}
+        assertEquals(expected, Day01().productOfTwo2020Parts(numbers));
+    }
+
+    @Test
+    fun productOfTwo2020PartsRealInput() {
+        val numbers =  Files.lines(Paths.get("./src/test/resources/day01.txt")).mapToInt{it.toInt()}.toList()
+        assertEquals(1013211, Day01().productOfTwo2020Parts(numbers));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = [
+        "1721, 979, 366, 299, 675, 1456; 241861950",
+        "1, 2018, 1; 2018",
+        "1010, 0, 1010; 0"
+    ], delimiter = ';')
+    fun productOfThree2020Parts(rawNumbers: String, expected : Int) {
+        val numbers = rawNumbers.split(", ").map{ it.toInt()}
+        assertEquals(expected, Day01().productOfThree2020Parts(numbers));
+    }
+
+    @Test
+    fun productOfThree2020PartsRealInput() {
+        val numbers =  Files.lines(Paths.get("./src/test/resources/day01.txt")).mapToInt{it.toInt()}.toList()
+        assertEquals(13891280, Day01().productOfThree2020Parts(numbers));
     }
 }
