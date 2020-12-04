6c115cbb280d8a90b954ba119c48b92be90c86f7
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-04 18:02:07 +0100

    Day3 part 2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -1,8 +1,8 @@
 class Day03 {
-    fun numberOfTreesInPath(map : List<String>, slopeRight: Int, slopeDown: Int) : Int {
+    fun numberOfTreesInPath(map : List<String>, slopeRight: Int, slopeDown: Int) : Long {
         var x = 0
         var y = 0
-        var trees = 0
+        var trees : Long = 0
         val mapWidth = map[0].length
         while (y < map.size) {
             if (map[y][x] == '#') {
--- src/test/kotlin/Day03Test.kt
+++ src/test/kotlin/Day03Test.kt
@@ -9,24 +9,63 @@ import kotlin.streams.toList
 internal class Day03Test {
 
     @ParameterizedTest
-    @CsvSource(value = ["..##.......\\n" +
-            "#...#...#..\\n" +
-            ".#....#..#.\\n" +
-            "..#.#...#.#\\n" +
-            ".#...##..#.\\n" +
-            "..#.##.....\\n" +
-            ".#.#.#....#\\n" +
-            ".#........#\\n" +
-            "#.##...#...\\n" +
-            "#...##....#\\n" +
-            ".#..#...#.#,3,1,7"], delimiter = ',')
-    fun numberOfTreesInPath(rawMap: String, slopeRight: Int, slopeDown : Int, expected : Int) {
+    @CsvSource(
+        value = ["..##.......\\n" +
+                "#...#...#..\\n" +
+                ".#....#..#.\\n" +
+                "..#.#...#.#\\n" +
+                ".#...##..#.\\n" +
+                "..#.##.....\\n" +
+                ".#.#.#....#\\n" +
+                ".#........#\\n" +
+                "#.##...#...\\n" +
+                "#...##....#\\n" +
+                ".#..#...#.#,3,1,7"], delimiter = ','
+    )
+    fun numberOfTreesInPath(rawMap: String, slopeRight: Int, slopeDown: Int, expected: Int) {
         assertEquals(expected, Day03().numberOfTreesInPath(rawMap.split("\\n"), slopeRight, slopeDown))
     }
 
     @Test
     fun realNumberOfTreesInPath() {
-        val map =  Files.lines(Paths.get("./src/test/resources/day03.txt")).toList()
+        val map = Files.lines(Paths.get("./src/test/resources/day03.txt")).toList()
         assertEquals(153, Day03().numberOfTreesInPath(map, 3, 1))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = ["..##.......\\n" +
+                "#...#...#..\\n" +
+                ".#....#..#.\\n" +
+                "..#.#...#.#\\n" +
+                ".#...##..#.\\n" +
+                "..#.##.....\\n" +
+                ".#.#.#....#\\n" +
+                ".#........#\\n" +
+                "#.##...#...\\n" +
+                "#...##....#\\n" +
+                ".#..#...#.#,336"], delimiter = ','
+    )
+    fun numberOfTreesInSeveralPaths(rawMap: String, expected: Int) {
+        val day03 = Day03()
+        val map = rawMap.split("\\n")
+        val numberOfTreesInPaths = day03.numberOfTreesInPath(map, 1, 1) *
+                    day03.numberOfTreesInPath(map, 3, 1) *
+                    day03.numberOfTreesInPath(map, 5, 1) *
+                    day03.numberOfTreesInPath(map, 7, 1) *
+                    day03.numberOfTreesInPath(map, 1, 2)
+        assertEquals(expected, numberOfTreesInPaths)
+    }
+
+    @Test
+    fun realNumberOfTreesInSeveralPaths() {
+        val map = Files.lines(Paths.get("./src/test/resources/day03.txt")).toList()
+        val day03 = Day03()
+        val numberOfTreesInPaths = day03.numberOfTreesInPath(map, 1, 1) *
+                day03.numberOfTreesInPath(map, 3, 1) *
+                day03.numberOfTreesInPath(map, 5, 1) *
+                day03.numberOfTreesInPath(map, 7, 1) *
+                day03.numberOfTreesInPath(map, 1, 2)
+        assertEquals(0, numberOfTreesInPaths)
     }
 }
