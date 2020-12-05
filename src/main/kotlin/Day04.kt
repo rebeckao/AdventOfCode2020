c0d74b7404aa54942ceb090c3bd01a874b1e5bd4
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-05 11:22:02 +0100

    Day 4 part 2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -16,4 +16,36 @@ class Day04 {
             .toSet()
         return availableCredentials.containsAll(requiredCredentials)
     }
+
+    fun passportIsReallyValid(passport: String) : Boolean {
+        val availableCredentials = passport.split("[\\s\\r\\n]+".toRegex())
+            .map { it.split(":") }
+            .filter { credentialIsValid(it[0], it[1]) }
+            .map { it[0] }
+            .toSet()
+        return availableCredentials.containsAll(requiredCredentials)
+    }
+
+    fun credentialIsValid(type: String, value: String): Boolean {
+        return when (type) {
+            "byr" -> value.matches("\\d{4}".toRegex()) && value.toInt() >= 1920 && value.toInt() <= 2002
+            "iyr" -> value.matches("\\d{4}".toRegex()) && value.toInt() >= 2010 && value.toInt() <= 2020
+            "eyr" -> value.matches("\\d{4}".toRegex()) && value.toInt() >= 2020 && value.toInt() <= 2030
+            "hgt" -> {
+                if (value.matches("\\d+cm".toRegex())) {
+                    val cm = value.substring(0, value.length - 2).toInt()
+                    return cm >= 150 && cm <= 193
+                } else if (value.matches("\\d+in".toRegex())) {
+                    val inches = value.substring(0, value.length - 2).toInt()
+                    return inches >= 59 && inches <= 76
+                } else {
+                    return false
+                }
+            }
+            "hcl" -> value.matches("#[0-9a-f]{6}".toRegex())
+            "ecl" -> value.matches("(amb)|(blu)|(brn)|(gry)|(grn)|(hzl)|(oth)".toRegex())
+            "pid" -> value.matches("[0-9]{9}".toRegex())
+            else -> true
+        }
+    }
 }
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -50,4 +50,67 @@ internal class Day04Test {
             .count()
         assertEquals(208, validPassports)
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "byr:2002, true",
+            "byr:2003, false",
+            "hgt:60in, true",
+            "hgt:190cm, true",
+            "hgt:190in, false",
+            "hgt:190, false",
+            "hcl:#123abc, true",
+            "hcl:#123abz, false",
+            "hcl:123abc, false",
+            "ecl:brn, true",
+            "ecl:wat, false",
+            "pid:000000001, true",
+            "pid:0123456789, false"
+        ]
+    )
+    fun credentialIsValid(rawCredential: String, expected: Boolean) {
+        val credential = rawCredential.split(":")
+        assertEquals(expected, Day04().credentialIsValid(credential[0], credential[1]))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "eyr:1972 cid:100\\n" +
+                    "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926, false",
+            "iyr:2019\\n" +
+                    "hcl:#602927 eyr:1967 hgt:170cm\\n" +
+                    "ecl:grn pid:012533040 byr:1946, false",
+            "hcl:dab227 iyr:2012\\n" +
+                    "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277, false",
+            "hgt:59cm ecl:zzz\\n" +
+                    "eyr:2038 hcl:74454a iyr:2023\\n" +
+                    "pid:3556412378 byr:2007, false",
+            "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\\n" +
+                    "hcl:#623a2f, true",
+            "eyr:2029 ecl:blu cid:129 byr:1989\\n" +
+                    "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm, true",
+            "hcl:#888785\\n" +
+                    "hgt:164cm byr:2001 iyr:2015 cid:88\\n" +
+                    "pid:545766238 ecl:hzl\\n" +
+                    "eyr:2022, true",
+            "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719, true"
+        ]
+    )
+    fun passportIsReallyValid(credentials: String, expected: Boolean) {
+        assertEquals(expected, Day04().passportIsReallyValid(credentials.replace("\\n", "\n")))
+    }
+
+    @Test
+    fun realNumberOfReallyValidPassports() {
+        val day04 = Day04()
+        val validPassports = Files.lines(Paths.get("./src/test/resources/day04.txt"))
+            .collect(joining("<delimeter>"))
+            .split("<delimeter><delimeter>")
+            .map { it.replace("<delimeter>", "\n") }
+            .filter { day04.passportIsReallyValid(it) }
+            .count()
+        assertEquals(208, validPassports)
+    }
 }
