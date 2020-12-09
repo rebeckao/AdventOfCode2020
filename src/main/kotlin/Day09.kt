757b3862dff2114ed4e934b18ebd8ec56c91c16b
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-09 23:12:00 +0100

    Day 9 Part 2

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -9,4 +9,27 @@ class Day09 {
         }
         return 0
     }
+
+    fun contiguousSetSummingTo(target: Long, numbers: List<Long>): Long {
+        for ((i, number) in numbers.withIndex()) {
+            var sum = number
+            var min = number
+            var max = number
+            var index = i
+            while (sum < target) {
+                index++
+                val newNumber = numbers[index]
+                sum += newNumber
+                if (newNumber < min) {
+                    min = newNumber
+                } else if (newNumber > max) {
+                    max = newNumber
+                }
+                if (sum == target) {
+                    return min + max
+                }
+            }
+        }
+        return 0
+    }
 }
--- src/test/kotlin/Day09Test.kt
+++ src/test/kotlin/Day09Test.kt
@@ -38,4 +38,37 @@ internal class Day09Test {
         val numbers = Files.lines(Paths.get("./src/test/resources/day09.txt")).map { it.toLong() }.toList()
         assertEquals(20874512, Day09().firstNumberNotMatching(numbers, 25))
     }
+
+    @Test
+    fun contiguousSetSummingTo() {
+        val numbers = listOf(
+            35,
+            20,
+            15,
+            25,
+            47,
+            40,
+            62,
+            55,
+            65,
+            95,
+            102,
+            117,
+            150,
+            182,
+            127,
+            219,
+            299,
+            277,
+            309,
+            576
+        ).map { it.toLong() }
+        assertEquals(62, Day09().contiguousSetSummingTo(127, numbers))
+    }
+
+    @Test
+    fun realContiguousSetSummingTo() {
+        val numbers = Files.lines(Paths.get("./src/test/resources/day09.txt")).map { it.toLong() }.toList()
+        assertEquals(3012420, Day09().contiguousSetSummingTo(20874512, numbers))
+    }
 }
