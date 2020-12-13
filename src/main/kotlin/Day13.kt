920a6e026b60a2552af6c6e536d903bcf13e3a3e
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-13 16:42:19 +0100

    Day 13 part 2

--- src/main/kotlin/Day13.kt
+++ src/main/kotlin/Day13.kt
@@ -1,8 +1,11 @@
+import java.math.BigInteger
+import kotlin.math.abs
+
 class Day13 {
     fun earliestDeparture(startWaiting: Int, allBuses: String): Int {
         var shortestWaitingTime = -1
         var bestBus = 0
-        val buses = allBuses.split(",").filter { !it.equals("x") }.map { it.toInt() }
+        val buses = allBuses.split(",").filter { it != "x" }.map { it.toInt() }
         for (bus in buses) {
             val minutesToWait = bus - (startWaiting % bus)
             if (shortestWaitingTime == -1 || minutesToWait < shortestWaitingTime) {
@@ -11,5 +14,65 @@ class Day13 {
             }
         }
         return bestBus * shortestWaitingTime
+    }
+
+    fun earliestTimestampForConsecutiveDepartures(allBuses: String): BigInteger {
+        val busMinutesAndNumbers = allBuses.split(",").withIndex().filter { it.value != "x" }
+            .map { Pair(it.index, it.value.toLong()) }
+        val modEquations = busMinutesAndNumbers.map { Pair(it.second - it.first, it.second) }
+        return chineseRemainderTheoremResult(modEquations)
+    }
+
+    /*
+        The solution to x = a_1 mod n_1, x = a_2 mod n_2 ... x = a_k mod n_k
+        modEquations: A list of pairs of a, n representing the equation x = a mod n
+        returns x
+     */
+    private fun chineseRemainderTheoremResult(modEquations: List<Pair<Long, Long>>): BigInteger {
+        val productOfAll = modEquations.map { it.second }.reduce { a, b -> a * b }
+        return modEquations.map { chineseRemainderTheoremPartForSingleEquation(it.first, it.second, productOfAll) }
+            .reduce { a, b -> a.plus(b) }
+            .mod(BigInteger.valueOf(productOfAll))
+    }
+
+    fun chineseRemainderTheoremPartForSingleEquation(aRaw: Long, n: Long, N: Long): BigInteger {
+        val a = positiveModulo(aRaw, n)
+        if (a == 0L) {
+            return BigInteger.ZERO
+        }
+        val productOfOthers = N / n
+        val eulerPhi = eulerPhi(n)
+        val b = b(productOfOthers, eulerPhi, n)
+        if ((b * productOfOthers) % n != 1L) {
+            throw IllegalStateException("b ($b) should be 1 mod n ($n)!")
+        }
+        return BigInteger.valueOf(a)
+            .multiply(BigInteger.valueOf(b))
+            .multiply(BigInteger.valueOf(N))
+            .divide(BigInteger.valueOf(n))
+    }
+
+    private fun positiveModulo(possiblyNegative: Long, mod: Long) =
+        ((possiblyNegative % mod) + mod) % mod
+
+    fun b(productOfOthers: Long, eulerPhi: Int, n: Long): Long {
+        return BigInteger.valueOf(productOfOthers)
+            .pow(eulerPhi - 1)
+            .mod(BigInteger.valueOf(n))
+            .toLong()
+    }
+
+    fun eulerPhi(n: Long): Int {
+        var relativePrimes = 0
+        for (number in 1..n) {
+            if (greatestCommonDivisorPreservingSign(number, n) == 1L) {
+                relativePrimes++
+            }
+        }
+        return relativePrimes
+    }
+
+    private fun greatestCommonDivisorPreservingSign(a: Long, b: Long): Long {
+        return if (b == 0L) abs(a) else greatestCommonDivisorPreservingSign(b, a % b)
     }
 }
--- src/test/kotlin/Day13Test.kt
+++ src/test/kotlin/Day13Test.kt
@@ -1,5 +1,8 @@
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import java.math.BigInteger
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.toList
@@ -16,5 +19,61 @@ internal class Day13Test {
     fun realEarliestDeparture() {
         val input = Files.lines(Paths.get("./src/test/resources/day13.txt")).toList()
         assertEquals(136, Day13().earliestDeparture(input[0].toInt(), input[1]))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "7,13,x,x,59,x,31,19;1068781",
+            "17,x,13,19;3417",
+            "67,7,59,61;754018",
+            "67,x,7,59,61;779210",
+            "67,7,x,59,61;1261476",
+            "1789,37,47,1889;1202161486"
+        ], delimiter = ';'
+    )
+    fun earliestTimestampForConsecutiveDepartures(rawBuses: String, expected: Long) {
+        assertEquals(BigInteger.valueOf(expected), Day13().earliestTimestampForConsecutiveDepartures(rawBuses))
+    }
+
+    @Test
+    fun realEarliestTimestampForConsecutiveDepartures() {
+        val input = Files.lines(Paths.get("./src/test/resources/day13.txt")).toList()
+        assertEquals(BigInteger("305068317272992"), Day13().earliestTimestampForConsecutiveDepartures(input[1]))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "3,2",
+        "33,20",
+        "48, 16",
+        "92, 44"
+    )
+    fun eulerPhi(n: Long, expected: Int) {
+        assertEquals(expected, Day13().eulerPhi(n))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2,3,210,140",
+        "3,7,210,360",
+        "3,10,210,63",
+        "0,10,210,0"
+    )
+    fun chineseRemainderTheoremPartForSingleEquation(a: Long, n: Long, N: Long, expected: Long) {
+        assertEquals(
+            BigInteger.valueOf(expected),
+            Day13().chineseRemainderTheoremPartForSingleEquation(a, n, N)
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "2,3,70,1",
+        "6,7,30,4",
+        "4,10,21,1"
+    )
+    fun b(eulerPhi: Int, n: Long, productOfOthers: Long, expected: Long) {
+        assertEquals(expected, Day13().b(productOfOthers, eulerPhi, n))
     }
 }
