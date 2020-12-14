a2472bc4e6affdb1ab738aa09797e1a0bbf98f63
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-14 21:43:48 +0100

    Day 14 part 2

--- src/main/kotlin/Day14.kt
+++ src/main/kotlin/Day14.kt
@@ -1,6 +1,6 @@
 class Day14 {
     private val maskPattern = Regex("mask = ([X10]+)")
-    private val memPattern = Regex("mem\\[([0-9]+)\\] = ([0-9]+)")
+    private val memPattern = Regex("mem\\[([0-9]+)] = ([0-9]+)")
 
     fun sumOfValuesAfterProgram(instructions: List<String>): Long {
         var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
@@ -35,5 +35,53 @@ class Day14 {
             mutableValue[index] = bit
         }
         return mutableValue.joinToString("")
+    }
+
+    fun sumOfValuesAfterProgramV2(instructions: List<String>): Long {
+        var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        val memory = mutableMapOf<String, Long>()
+        var sum = 0L
+        for (instruction in instructions) {
+            val maskMatch = maskPattern.matchEntire(instruction)
+            if (maskMatch != null) {
+                mask = maskMatch.groupValues[1]
+            } else if (memPattern.matches(instruction)) {
+                val memMatch = memPattern.matchEntire(instruction)!!.groupValues
+                val target = memMatch[1]
+                    .toInt()
+                    .toString(2)
+                    .padStart(36, '0')
+                val value = memMatch[2].toLong()
+                val maskedTargets = applyMaskV2(mask, target)
+                for (maskedTarget in maskedTargets) {
+                    if (memory.containsKey(maskedTarget)) {
+                        sum -= memory[maskedTarget]!!
+                    }
+                    memory[maskedTarget] = value
+                }
+                sum += maskedTargets.size * value
+            } else {
+                throw IllegalStateException("Not recognized: $instruction")
+            }
+        }
+        return sum
+    }
+
+    fun applyMaskV2(mask: String, value: String): Set<String> {
+        val mutableValue = value.toCharArray()
+        for ((index, bit) in mask.withIndex()) {
+            if (bit == '0') {
+                continue
+            } else if (bit == '1') {
+                mutableValue[index] = '1'
+            } else {
+                val valueSoFar = mutableValue.slice(0 until index).joinToString("")
+                val restOfMask = mask.substring(index + 1)
+                val restOfValue = value.substring(index + 1)
+                val possibilities = applyMaskV2(restOfMask, restOfValue)
+                return possibilities.flatMap { setOf(valueSoFar + "1" + it, valueSoFar + "0" + it) }.toSet()
+            }
+        }
+        return setOf(mutableValue.joinToString(""))
     }
 }
--- src/test/kotlin/Day14Test.kt
+++ src/test/kotlin/Day14Test.kt
@@ -26,4 +26,35 @@ internal class Day14Test {
         val instructions = Files.lines(Paths.get("./src/test/resources/day14.txt")).toList()
         assertEquals(11501064782628, Day14().sumOfValuesAfterProgram(instructions))
     }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "101010;X1001X;011010,011011,111010,111011",
+            "11010;0X0XX;10000,10001,10010,10011,11000,11001,11010,11011",
+            "000000000000000000000000000000101010;000000000000000000000000000000X1001X;000000000000000000000000000000011010,000000000000000000000000000000011011,000000000000000000000000000000111010,000000000000000000000000000000111011",
+        ], delimiter = ';'
+    )
+    fun applyMaskV2(value: String, mask: String, expected: String) {
+        assertEquals(expected.split(",").toSet(), Day14().applyMaskV2(mask, value).toSet())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "mask = 000000000000000000000000000000X1001X%" +
+                    "mem[42] = 100%" +
+                    "mask = 00000000000000000000000000000000X0XX%" +
+                    "mem[26] = 1, 208"
+        ]
+    )
+    fun sumOfValuesAfterProgramV2(instructions: String, expected: Long) {
+        assertEquals(expected, Day14().sumOfValuesAfterProgramV2(instructions.split("%")))
+    }
+
+    @Test
+    fun realSumOfValuesAfterProgramV2() {
+        val instructions = Files.lines(Paths.get("./src/test/resources/day14.txt")).toList()
+        assertEquals(5142195937660, Day14().sumOfValuesAfterProgramV2(instructions))
+    }
 }
