79c6d68ec8f5a631516293988c410e11a44504c1
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-16 19:07:09 +0100

    Day 16 part 2

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -1,20 +1,67 @@
 class Day16 {
-    val rulePattern = Regex("(.+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)")
+    private val rulePattern = Regex("(.+): ([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)")
 
     fun errorScanningRate(rules: List<String>, tickets: List<String>): Int {
-        val parsedRules = rules.map { rulePattern.matchEntire(it) }
-            .filter { it != null }
-            .map { it!!.groupValues }
-            .map { Rule(it[2].toInt(), it[3].toInt(), it[4].toInt(), it[5].toInt()) }
+        val parsedRules = parseRules(rules)
         return tickets.flatMap { it.split(",") }
             .map { it.toInt() }
             .filter { value -> !parsedRules.any { rule -> rule.matches(value) } }
             .sum()
     }
 
-    private data class Rule(val min1: Int, val max1: Int, val min2: Int, val max2: Int) {
+    private fun parseRules(rules: List<String>): List<Rule> {
+        return rules.map { rulePattern.matchEntire(it) }
+            .filter { it != null }
+            .map { it!!.groupValues }
+            .map { Rule(it[1], it[2].toInt(), it[3].toInt(), it[4].toInt(), it[5].toInt()) }
+    }
+
+    private data class Rule(val name: String, val min1: Int, val max1: Int, val min2: Int, val max2: Int) {
         fun matches(value: Int): Boolean {
             return (value in min1..max1) || (value in min2..max2)
         }
+    }
+
+    fun departureFields(rules: List<String>, tickets: List<String>, rawTicket: String): Long {
+        val ticket = rawTicket.split(",").map { it.toLong() }
+        var productOfDepartureFields = 1L
+        val identifiedFields = identifiedFields(rules, tickets)
+        for ((index, field) in identifiedFields.withIndex()) {
+            if (field.startsWith("departure")) {
+                productOfDepartureFields *= ticket[index]
+            }
+        }
+        return productOfDepartureFields
+    }
+
+    fun identifiedFields(rules: List<String>, tickets: List<String>): List<String> {
+        val parsedRules = parseRules(rules)
+        val validTickets = tickets.map { it.split(",").map { it.toInt() } }
+            .filter { isValid(it, parsedRules) }
+        val matchingFields = mutableMapOf<Int, MutableSet<String>>()
+        for (i in validTickets[0].indices) {
+            matchingFields[i] = findAllMatchingRules(parsedRules, validTickets, i)
+        }
+        while (matchingFields.values.map { it.size }.any { it > 1 }) {
+            val alreadyIdentifiedFields = matchingFields.values.filter { it.size == 1 }.map { it.first() }
+            for (possibleFields in matchingFields.values.filter { it.size > 1 }) {
+                possibleFields.removeAll(alreadyIdentifiedFields)
+            }
+        }
+        return matchingFields.entries.sortedBy { it.key }.map { it.value.first() }
+    }
+
+    private fun findAllMatchingRules(
+        parsedRules: List<Rule>,
+        validTickets: List<List<Int>>,
+        i: Int
+    ): MutableSet<String> {
+        return parsedRules.filter { rule -> !validTickets.map { it[i] }.any { !rule.matches(it) } }
+            .map { it.name }
+            .toMutableSet()
+    }
+
+    private fun isValid(ticket: List<Int>, parsedRules: List<Rule>): Boolean {
+        return !ticket.any { value -> !parsedRules.any { rule -> rule.matches(value) } }
     }
 }
--- src/test/kotlin/Day16Test.kt
+++ src/test/kotlin/Day16Test.kt
@@ -1,6 +1,7 @@
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.toList
@@ -28,5 +29,38 @@ internal class Day16Test {
         val rules = Files.lines(Paths.get("./src/test/resources/day16_rules.txt")).toList()
         val tickets = Files.lines(Paths.get("./src/test/resources/day16_nearby_tickets.txt")).toList()
         assertEquals(25895, Day16().errorScanningRate(rules, tickets))
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        value = [
+            "class: 1-3 or 5-7%" +
+                    "row: 6-11 or 33-44%" +
+                    "seat: 13-40 or 45-50;" +
+                    "7,3,47%" +
+                    "40,4,50%" +
+                    "55,2,20%" +
+                    "38,6,12; row,class,seat",
+            "class: 0-1 or 4-19%" +
+                    "row: 0-5 or 8-19%" +
+                    "seat: 0-13 or 16-19;" +
+                    "3,9,18%" +
+                    "15,1,5%" +
+                    "5,14,9; row,class,seat"
+        ], delimiter = ';'
+    )
+    fun identifiedFields(rawRules: String, rawTickets: String, rawExpected: String) {
+        val rules = rawRules.split("%")
+        val tickets = rawTickets.split("%")
+        val expected = rawExpected.split(",")
+        assertEquals(expected, Day16().identifiedFields(rules, tickets))
+    }
+
+    @Test
+    fun realDepartureFields() {
+        val rules = Files.lines(Paths.get("./src/test/resources/day16_rules.txt")).toList()
+        val tickets = Files.lines(Paths.get("./src/test/resources/day16_nearby_tickets.txt")).toList()
+        val rawTicket = "61,151,137,191,59,163,89,83,71,179,67,149,197,167,181,173,53,139,193,157"
+        assertEquals(5865723727753, Day16().departureFields(rules, tickets, rawTicket))
     }
 }
