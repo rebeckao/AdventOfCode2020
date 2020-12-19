86fc0186fd3eed81a6d1536b35b3e6048b0f1382
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-19 10:42:28 +0100

    Day 18 part 2

--- src/test/kotlin/Day18.kt
+++ src/test/kotlin/Day18.kt
@@ -44,4 +44,60 @@ class Day18 {
         }
         return Pair(index, resultSoFar)
     }
+
+    fun evaluateExpressionPlusPrecedence(rawExpression: String): Long {
+        val expression = rawExpression.replace(" ", "")
+        return evaluateInnerExpressionPlusPrecedence(0, expression).second
+    }
+
+    private fun evaluateInnerExpressionPlusPrecedence(initialIndex: Int, expression: String): Pair<Int, Long> {
+        var resultSoFar = 0L
+        var waitingToBeMultiplied = 1L
+        var isPlus = true
+        var index = initialIndex
+        while (index < expression.length) {
+            val currentElement = expression[index]
+            if (currentElement == ')') {
+                return Pair(index, resultSoFar)
+            }
+            if (currentElement == '+') {
+                isPlus = true
+                index++
+                continue
+            }
+            if (currentElement == '*') {
+                isPlus = false
+                index++
+                continue
+            }
+
+            var value: Long
+            if (currentElement == '(') {
+                index++
+                val result = evaluateInnerExpressionPlusPrecedence(index, expression)
+                index = result.first
+                value = result.second
+            } else {
+                value = Character.getNumericValue(currentElement).toLong()
+            }
+
+            if (isPlus) {
+                resultSoFar += value
+                if (index + 1 >= expression.length || expression[index + 1] != '+') {
+                    resultSoFar *= waitingToBeMultiplied
+                    waitingToBeMultiplied = 1L
+                }
+            } else {
+                if (index + 1 < expression.length && expression[index + 1] == '+') {
+                    waitingToBeMultiplied *= resultSoFar
+                    resultSoFar = value
+                } else {
+                    resultSoFar *= value
+                }
+            }
+
+            index++
+        }
+        return Pair(index, resultSoFar)
+    }
 }
--- src/test/kotlin/Day18Test.kt
+++ src/test/kotlin/Day18Test.kt
@@ -1,4 +1,3 @@
-
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
@@ -24,6 +23,31 @@ internal class Day18Test {
     @Test
     fun realEvaluateExpressionLeftPrecedence() {
         val day18 = Day18()
-        assertEquals(31142189909908, Files.lines(Paths.get("./src/test/resources/day18.txt")).mapToLong { day18.evaluateExpressionLeftPrecedence(it) }.sum())
+        assertEquals(31142189909908,
+            Files.lines(Paths.get("./src/test/resources/day18.txt"))
+                .mapToLong { day18.evaluateExpressionLeftPrecedence(it) }.sum()
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "1 + 2 * 3 + 4 * 5 + 6, 231",
+        "1 + (2 * 3) + (4 * (5 + 6)), 51",
+        "2 * 3 + (4 * 5), 46",
+        "5 + (8 * 3 + 9 + 3 * 4 * 3), 1445",
+        "5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)), 669060",
+        "((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2, 23340"
+    )
+    fun evaluateExpressionPlusPrecedence(expression: String, expected: Long) {
+        assertEquals(expected, Day18().evaluateExpressionPlusPrecedence(expression))
+    }
+
+    @Test
+    fun realEvaluateExpressionPlusPrecedence() {
+        val day18 = Day18()
+        assertEquals(323912478287549,
+            Files.lines(Paths.get("./src/test/resources/day18.txt"))
+                .mapToLong { day18.evaluateExpressionPlusPrecedence(it) }.sum()
+        )
     }
 }
