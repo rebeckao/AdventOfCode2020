747e2ad74f8de61bf2e3dcfe53172a6375b9938e
rebeckao <rebecka.oefverholm@gmail.com>
2020-12-19 20:49:50 +0100

    Day 19 part 2

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -2,11 +2,7 @@ class Day19 {
     private val leafRulePattern = Regex("\"(\\w)\"")
 
     fun rulesToRegex(rawRules: List<String>): String {
-        val rules = rawRules
-            .map { it.split(":") }
-            .sortedBy { it[0] }
-            .map { Pair(it[0].toInt(), it[1].trim()) }
-            .toMap()
+        val rules = parseRules(rawRules)
         return "^${toRegex(rules[0]!!, rules)}$"
     }
 
@@ -26,5 +22,47 @@ class Day19 {
             .map { it.toInt() }
             .map { rules[it] }
             .joinToString("") { toRegex(it!!, rules) }
+    }
+
+    fun parseRules(rawRules: List<String>): Map<Int, String> {
+        return rawRules
+            .map { it.split(":") }
+            .sortedBy { it[0] }
+            .map { Pair(it[0].toInt(), it[1].trim()) }
+            .toMap()
+    }
+
+    fun toModifiedRegex(currentRule: String, rules: Map<Int, String>, i: Int): String {
+        if (i == 8) {
+            return "(${toModifiedRegex(rules[42]!!, rules, 42)})+"
+        }
+        if (i == 11) {
+            val rule42 = toModifiedRegex(rules[42]!!, rules, 42)
+            val rule31 = toModifiedRegex(rules[31]!!, rules, 31)
+            return "(" +
+                    "($rule42$rule31)" +
+                    "|($rule42$rule42$rule31$rule31)" +
+                    "|($rule42$rule42$rule42$rule31$rule31$rule31)" +
+                    "|($rule42$rule42$rule42$rule42$rule31$rule31$rule31$rule31)" +
+                    "|($rule42$rule42$rule42$rule42$rule42$rule31$rule31$rule31$rule31$rule31)" +
+                    ")"
+        }
+
+        val matchResult = leafRulePattern.matchEntire(currentRule)
+        if (matchResult != null) {
+            return matchResult.groupValues[1]
+        }
+
+        if (currentRule.contains("|")) {
+            val parts = currentRule.split("|")
+            val part1 = toModifiedRegex(parts[0], rules, -1)
+            val part2 = toModifiedRegex(parts[1], rules, -1)
+            return "(($part1)|($part2))"
+        }
+
+        return currentRule.trim()
+            .split(" ")
+            .map { it.toInt() }
+            .joinToString("") { toModifiedRegex(rules[it]!!, rules, it) }
     }
 }
--- src/test/kotlin/Day19Test.kt
+++ src/test/kotlin/Day19Test.kt
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -31,7 +32,156 @@ internal class Day19Test {
         val regex = Regex(Day19().rulesToRegex(rules))
         assertEquals(
             187,
-            Files.lines(Paths.get("./src/test/resources/day19_messages.txt")).filter { regex.matches(it) }.count()
+            Files.lines(Paths.get("./src/test/resources/day19_messages.txt"))
+                .filter { regex.matches(it) }.count()
+        )
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "0: 1 2;" +
+                "1: \"a\";" +
+                "2: 1 3 | 3 1;" +
+                "3: \"b\"," +
+                "aab;" +
+                "aba;" +
+                "aaa," +
+                "2",
+        "0: 4 1 5;" +
+                "1: 2 3 | 3 2;" +
+                "2: 4 4 | 5 5;" +
+                "3: 4 5 | 5 4;" +
+                "4: \"a\";" +
+                "5: \"b\"," +
+                "ababbb;" +
+                "bababa;" +
+                "abbbab;" +
+                "aaabbb;" +
+                "aaaabbb," +
+                "2",
+        "42: 9 14 | 10 1;" +
+                "9: 14 27 | 1 26;" +
+                "10: 23 14 | 28 1;" +
+                "1: \"a\";" +
+                "11: 42 31;" +
+                "5: 1 14 | 15 1;" +
+                "19: 14 1 | 14 14;" +
+                "12: 24 14 | 19 1;" +
+                "16: 15 1 | 14 14;" +
+                "31: 14 17 | 1 13;" +
+                "6: 14 14 | 1 14;" +
+                "2: 1 24 | 14 4;" +
+                "0: 8 11;" +
+                "13: 14 3 | 1 12;" +
+                "15: 1 | 14;" +
+                "17: 14 2 | 1 7;" +
+                "23: 25 1 | 22 14;" +
+                "28: 16 1;" +
+                "4: 1 1;" +
+                "20: 14 14 | 1 15;" +
+                "3: 5 14 | 16 1;" +
+                "27: 1 6 | 14 18;" +
+                "14: \"b\";" +
+                "21: 14 1 | 1 14;" +
+                "25: 1 1 | 1 14;" +
+                "22: 14 14;" +
+                "8: 42;" +
+                "26: 14 22 | 1 20;" +
+                "18: 15 15;" +
+                "7: 14 5 | 1 21;" +
+                "24: 14 1," +
+                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa;" +
+                "bbabbbbaabaabba;" +
+                "babbbbaabbbbbabbbbbbaabaaabaaa;" +
+                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa;" +
+                "bbbbbbbaaaabbbbaaabbabaaa;" +
+                "bbbababbbbaaaaaaaabbababaaababaabab;" +
+                "ababaaaaaabaaab;" +
+                "ababaaaaabbbaba;" +
+                "baabbaaaabbaaaababbaababb;" +
+                "abbbbabbbbaaaababbbbbbaaaababb;" +
+                "aaaaabbaabaaaaababaa;" +
+                "aaaabbaaaabbaaa;" +
+                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa;" +
+                "babaaabbbaaabaababbaabababaaab;" +
+                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba," +
+                "3"
+    )
+    fun numberOfMatchingMessages(rawRules: String, rawMessages: String, expected: Int) {
+        val regex = Regex(Day19().rulesToRegex(rawRules.split(";")))
+        assertEquals(expected, rawMessages.split(";").filter { regex.matches(it) }.count())
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "42: 9 14 | 10 1;" +
+                "9: 14 27 | 1 26;" +
+                "10: 23 14 | 28 1;" +
+                "1: \"a\";" +
+                "11: 42 31;" +
+                "5: 1 14 | 15 1;" +
+                "19: 14 1 | 14 14;" +
+                "12: 24 14 | 19 1;" +
+                "16: 15 1 | 14 14;" +
+                "31: 14 17 | 1 13;" +
+                "6: 14 14 | 1 14;" +
+                "2: 1 24 | 14 4;" +
+                "0: 8 11;" +
+                "13: 14 3 | 1 12;" +
+                "15: 1 | 14;" +
+                "17: 14 2 | 1 7;" +
+                "23: 25 1 | 22 14;" +
+                "28: 16 1;" +
+                "4: 1 1;" +
+                "20: 14 14 | 1 15;" +
+                "3: 5 14 | 16 1;" +
+                "27: 1 6 | 14 18;" +
+                "14: \"b\";" +
+                "21: 14 1 | 1 14;" +
+                "25: 1 1 | 1 14;" +
+                "22: 14 14;" +
+                "8: 42;" +
+                "26: 14 22 | 1 20;" +
+                "18: 15 15;" +
+                "7: 14 5 | 1 21;" +
+                "24: 14 1," +
+                "abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa;" +
+                "bbabbbbaabaabba;" +
+                "babbbbaabbbbbabbbbbbaabaaabaaa;" +
+                "aaabbbbbbaaaabaababaabababbabaaabbababababaaa;" +
+                "bbbbbbbaaaabbbbaaabbabaaa;" +
+                "bbbababbbbaaaaaaaabbababaaababaabab;" +
+                "ababaaaaaabaaab;" +
+                "ababaaaaabbbaba;" +
+                "baabbaaaabbaaaababbaababb;" +
+                "abbbbabbbbaaaababbbbbbaaaababb;" +
+                "aaaaabbaabaaaaababaa;" +
+                "aaaabbaaaabbaaa;" +
+                "aaaabbaabbaaaaaaabbbabbbaaabbaabaaa;" +
+                "babaaabbbaaabaababbaabababaaab;" +
+                "aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba," +
+                "12"
+    )
+    fun numberOfMatchingMessagesWithModification(rawRules: String, rawMessages: String, expected: Int) {
+        val rules = Day19().parseRules((rawRules.split(";")))
+        val regex = "^${Day19().toModifiedRegex(rules[0]!!, rules, 0)}$"
+        assertFalse(Regex(regex).matchEntire("aaaabbaaaabbaaa") != null)
+
+        val matchingMessages = rawMessages
+            .split(";")
+            .filter { Regex(regex).matchEntire(it) != null }
+
+        assertEquals(expected, matchingMessages.count())
+    }
+
+    @Test
+    fun realNumberOfMessagesThatMatchWithModifications() {
+        val rules = Day19().parseRules(Files.lines(Paths.get("./src/test/resources/day19_rules.txt")).toList())
+        val regex = "^${Day19().toModifiedRegex(rules[0]!!, rules, 0)}$"
+        assertEquals(
+            392,
+            Files.lines(Paths.get("./src/test/resources/day19_messages.txt"))
+                .filter { Regex(regex).matchEntire(it) != null }.count()
         )
     }
 }
